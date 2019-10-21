43123abe6d78548ed009fc0b3caade39aec1fd4b
yenny <aldald73@naver.com>
2019-10-21 21:03:53 +0900

    login UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Navbar">
+        <img src="logo.png" alt='logo' className="Logo"></img>
+      </div>
+      <div className="Container">
+        <text className="Title">로그인</text>
+        <text className="InputTitle">ID</text>
+        <input type="text" placeholder="아이디를 입력하세요" className="Input"></input>
+        <text className="InputTitle">Password</text>
+        <input type="password" placeholder="비밀번호를 입력하세요" className="Input"></input>
+        <button className="FindIDPW">아이디/비밀번호 찾기</button>
+        <button className="Login">로그인</button>
+        <button className="SignUp">회원가입</button>
+      </div>
     </div>
   );
 }
