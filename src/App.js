602f2c8f151b8f90af4e5b7981a42394984cd0ef
yenny <aldald73@naver.com>
2019-10-24 09:02:17 +0900

    logout & patient

--- src/App.js
+++ src/App.js
@@ -8,6 +8,29 @@ class App extends React.Component {
   state={
     login: false
   }
+
+  render() {
+    return (
+      this.state.login ? <Main 
+      token={this.state.token} name={this.state.name} role={this.state.role} 
+      _logout={this._logout.bind(this)}></Main> :
+      <div>
+        <div className="Navbar">
+          <img src="logo.png" alt='logo' className="Logo"></img>
+        </div>
+        <div className="Container">
+          <p className="Title">로그인</p>
+          <p className="InputTitle">ID</p>
+          <input type="text" placeholder="   아이디를 입력하세요" onChange={(e)=>{id=e.target.value;}} className="Input"></input>
+          <p className="InputTitle">Password</p>
+          <input type="password" placeholder="   비밀번호를 입력하세요" onChange={(e)=>{password=e.target.value;}} className="Input"></input>
+          <button className="FindIDPW">아이디/비밀번호 찾기</button>
+          <button onClick={this._loginAuth} className="Login">로그인</button>
+          <button className="SignUp">회원가입</button>
+        </div>
+      </div>
+    );
+  }
   _loginAuth = () => {
     if (id === undefined || id === "" || password === undefined || password === "") {
       alert("아이디와 비밀번호를 입력해주세요");
@@ -33,25 +56,8 @@ class App extends React.Component {
         .catch(err => {console.log(err); alert("아이디 또는 비밀번호가 틀렸습니다") });
     }
   }
-  render() {
-    return (
-      this.state.login ? <Main token={this.state.token} name={this.state.name} role={this.state.role}></Main> :
-      <div>
-        <div className="Navbar">
-          <img src="logo.png" alt='logo' className="Logo"></img>
-        </div>
-        <div className="Container">
-          <p className="Title">로그인</p>
-          <p className="InputTitle">ID</p>
-          <input type="text" placeholder="   아이디를 입력하세요" onChange={(e)=>{id=e.target.value;}} className="Input"></input>
-          <p className="InputTitle">Password</p>
-          <input type="password" placeholder="   비밀번호를 입력하세요" onChange={(e)=>{password=e.target.value;}} className="Input"></input>
-          <button className="FindIDPW">아이디/비밀번호 찾기</button>
-          <button onClick={this._loginAuth} className="Login">로그인</button>
-          <button className="SignUp">회원가입</button>
-        </div>
-      </div>
-    );
+  _logout = () =>{
+    this.setState({login: false})
   }
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './Main.css';
 import Popup from './Popup';
 
@@ -12,11 +13,21 @@ class Main extends React.Component {
         let Dr = null;
         if (this.props.role === "ROLE_Doctor")
             Dr = <div className="Flag">Dr.</div>
+
+        let CurPatient = null;
+        if (this.props.role === "ROLE_Doctor" && this.state.patient !== "")
+            CurPatient =
+                <div className="CurPatient">
+                    <div><b>{this.state.patient}</b>님 치료 진행중</div>
+                    <button onClick={() => {if (window.confirm('환자를 삭제하시겠습니까?')) this._patientDelete()}}><img src="./close.png" alt="close" className="CloseImg"></img></button>
+                </div>
+
         return (
             <div>
                 {Dr}
                 <div className="Navbar">
                     <img src="logo.png" alt='logo' className="Logo"></img>
+                    <button onClick={() =>{if (window.confirm('로그아웃 하시겠습니까?')) this.props._logout()}}><div>L O G O U T</div></button>
                 </div>
 
                 <div className="Container2">
@@ -30,6 +41,7 @@ class Main extends React.Component {
                         <div className="Hello">
                             안녕하세요,⠀<b>{this.props.name}</b>님
                         </div>
+                        {CurPatient}
                         <div className="Search">
                             <img src="searchIcon.png" alt="search" className="SearchIcon"></img>
                             <input type="text" placeholder="VR 컨텐츠의 이름을 검색해보세요!" className="SearchInput"></input>
@@ -47,20 +59,48 @@ class Main extends React.Component {
         if (this.props.role !== "ROLE_Doctor" || this.state.patient !== "") {
             //측정 실행
             console.log("measure start!");
+
+
         }
         else {//팝업창 실행
             this.setState({ showPopup: !this.state.showPopup });
         }
     }
 
-    _patientInfo = (ID) => {
-        //ID가 올바른지 유효 검사 필요
-        
-        //ID가 올바르지 않은 경우
-        //ID가 올바르지 않습니다 팝업창 출력
+    _patientDelete = () => {
+        //환자 삭제
+        axios({
+            method: 'DELETE',
+            url: `/Chart?id=${this.state.patient}`,
+            headers: {
+                'accept': 'application/json',
+                'X-AUTH-TOKEN': this.props.token
+            },
+            data: {
+                id: this.state.patient
+            }
+        }).then(json => { this.setState({ patient: "" }); console.log("patient deleted!") })
+            .catch(err => console.log(err));
+    }
 
-        //ID가 올바른 경우
-        this.setState({ showPopup: !this.state.showPopup, patient: ID },() => {this._measure();});
+    _patientInfo = (ID) => {
+        //ID가 올바른지 유효 검사
+        axios({
+            method: 'post',
+            url: `/Chart?id=${ID}`,
+            headers: {
+                'accept': 'application/json',
+                'X-AUTH-TOKEN': this.props.token
+            },
+            data: {
+                id: ID
+            }
+        }).then(json => {
+            if (json.data.result)
+                this.setState({ showPopup: !this.state.showPopup, patient: ID }, () => { this._measure() })
+            else alert("아이디가 올바르지 않습니다")
+        })
+            .catch(err => console.log(err));
     }
 }
 export default Main;
